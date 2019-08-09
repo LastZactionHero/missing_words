9b37681308b6b1faf19571e356992d5669837164
Zach Dicklin <zach@localhost.localdomain>
2019-08-08 23:15:44 -0600

    API requests

---
+++ README.md
@@ -0,0 +1,14 @@
+# Missing Words
+
+My Japanese flashcard app is missing some words. Where'd they go?
+
+- Download the full list from the API
+- Find what should be there from the CSV
+- Find missing words
+- Post 'em.
+
+In Rust, cause why not.
+
+## Where you left off:
+
+Able to fetch all terms from the API. They're in an enum data type, I'm unsure how to use the data, as it seems like an unusual enum.
--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,46 @@ static CSV_FILENAME: &str = "./data/word_list.csv";
 static ROWS_MAX: usize = 300;
 
 #[derive(Serialize,Deserialize, Debug)]
-struct TermApiResponse {
-    uuid: String
+#[serde(untagged)]
+enum TermApiResponse {
+    Array(Vec<TermApi>)
 }
 
-impl RestPath<()> for TermApi {
+#[derive(Serialize,Deserialize, Debug)]
+struct TermApi {
+    uuid: String,
+    scheduled_for: String,
+    scheduling_interval: u32,
+    term_a: String,
+    term_b: String,
+    meta: TermApiMeta
+}
+
+#[derive(Serialize,Deserialize, Debug)]
+struct TermApiMeta {
+    core_index: String,
+    jlpt: String,
+    new_opt_voc_index: String,
+    opt_sen_index: String,
+    opt_voc_index: String,
+    sent_ko_index: String,
+    sentence_cloze: String,
+    sentence_expression: String,
+    sentence_furigana: String,
+    sentence_kana: String,
+    sentence_meaning: String,
+    vocab_expression: String,
+    vocab_furigana: String,
+    vocab_ko_index: String,
+    vocab_pos: String
+}
+
+impl RestPath<()> for TermApiResponse {
     fn get_path(_: ()) -> Result<String,restson::Error> { Ok(String::from("/dev/terms")) }
 }
 
 #[derive(Deserialize, Debug)]
-struct Term {
+struct TermCsv {
     core_index: u32,
     vocab_ko_index: u32,
     sent_ko_index: u32,
@@ -45,20 +75,21 @@ struct Term {
 
 fn main() {
     // Load expected terms from CSV
-    let mut expected_terms: Vec<Term> = Vec::new();
+    let mut expected_terms: Vec<TermCsv> = Vec::new();
     read_csv(&mut expected_terms).expect("Could not read CSV");
 
     // Load current terms from API
     let mut client = RestClient::new("https://dyvnth6y4j.execute-api.us-east-1.amazonaws.com").unwrap();
-    let data: TermApi = client.get(()).unwrap();
+    let data: TermApiResponse = client.get(()).unwrap();
+
     println!("{:?}", data);
 }
 
-fn read_csv(terms: &mut Vec<Term>) -> Result<(), Box<dyn Error>> {
+fn read_csv(terms: &mut Vec<TermCsv>) -> Result<(), Box<dyn Error>> {
     let mut reader = Reader::from_path(CSV_FILENAME)?;
 
     for (i, row) in reader.records().enumerate() {
-        let term: Term = row?.deserialize(None)?;
+        let term: TermCsv = row?.deserialize(None)?;
         terms.push(term);
 
         if (i+1) == ROWS_MAX {
